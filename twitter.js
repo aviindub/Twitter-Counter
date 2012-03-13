e82cb5188175f911c904ba05509cdd5f3a91deb0
Avi Goldberg <aviindub@gmail.com>
2012-03-13 10:23:18 -0400

    cleaned up unnecessary checks and initializations for redis counts

--- twitter.js
+++ twitter.js
@@ -1,47 +1,7 @@
 var twitter = require('ntwitter');
 var redis = require('redis');
 var credentials = require('./credentials.js');
-
-//create redis client                                                                                                                                                                                                                       
 var client = redis.createClient();
-
-//if the 'awesome' key doesn't exist, create it                                                                                                                                                                                             
-client.exists('awesome', function(error, exists) {
-    if(error) {
-        console.log('ERROR: '+error);
-    } else if(!exists) {
-        client.set('awesome', 0); //create the awesome key
-    };
-});
-client.exists('cool', function(error, exists) {
-    if(error) {
-        console.log('ERROR: '+error);
-    } else if(!exists) {
-        client.set('cool', 0); //create the awesome key
-    };
-});
-client.exists('rad', function(error, exists) {
-    if(error) {
-        console.log('ERROR: '+error);
-    } else if(!exists) {
-        client.set('rad', 0); //create the awesome key
-    };
-});
-client.exists('gnarly', function(error, exists) {
-    if(error) {
-        console.log('ERROR: '+error);
-    } else if(!exists) {
-        client.set('gnarly', 0); //create the awesome key
-    };
-});
-client.exists('groovy', function(error, exists) {
-    if(error) {
-        console.log('ERROR: '+error);
-    } else if(!exists) {
-        client.set('groovy', 0); //create the awesome key
-    };
-});
-
 
 var t = new twitter({
     consumer_key: credentials.consumer_key,
@@ -52,11 +12,10 @@ var t = new twitter({
 
 t.stream(
     'statuses/filter',
-    { track: ['awesome', 'cool', 'rad', 'gnarly', 'groovy'] },
-    function(stream) {
+    { track: ['awesome', 'cool', 'rad', 'gnarly', 'groovy'] }, //words to be tracked by twitter stream
+    function(stream) { //check for each tracked word in the tweet and incr redis count if found
         stream.on('data', function(tweet) {
-            console.log(tweet.text);
-            //if awesome is in the tweet text, increment the counter                                                                                                                                                                        
+            console.log(tweet.text);                                                                        
             if(tweet.text.match(/awesome/)) {
                 client.incr('awesome');
             }
